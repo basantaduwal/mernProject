8f313ead4d0bff9b0ba7a5ab337cd82170121426
basantaduwal <basantaduwal10@gmail.com>
2026-08-04 08:02:48 +0545

    auth layout

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import useCart from '../hooks/useCart';
@@ -200,14 +200,20 @@ const Footer = () => (
   </footer>
 );
 
-const MainLayout = () => (
-  <div className="flex flex-col min-h-screen">
-    <Navbar />
-    <main className="flex-1">
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
-);
+const MainLayout = () => {
+  const location = useLocation();
+  const hideNavFooter = ['/login', '/register'].includes(location.pathname);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!hideNavFooter && <Navbar />}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      {!hideNavFooter && <Footer />}
+    </div>
+  );
+};
+
 
 export default MainLayout;
